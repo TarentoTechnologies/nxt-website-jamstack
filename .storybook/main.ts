2baea608804802b64989a3bd1f7f33daffc3d102
Abhijith K <abhijith.kizhakkethodi@tarento.com>
2023-08-17 17:21:44 +0530

    WIP: story-buttons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,3 @@
-// import type { StorybookConfig } from "@storybook/react-webpack5";
-
-// const config: StorybookConfig = {
-//   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-//   addons: [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/addon-onboarding",
-//     "@storybook/addon-interactions",
-//     "storybook-css-modules",
-//   ],
-//   framework: {
-//     name: "@storybook/react-webpack5",
-//     options: {},
-//   },
-//   docs: {
-//     autodocs: "tag",
-//   },
-// };
-// export default config;
-
 import type { StorybookConfig } from "@storybook/react-webpack5";
 import path from "path";
 
@@ -36,6 +15,49 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  core: {
+    builder: {
+      name: "@storybook/builder-webpack5",
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
+  },
+  webpackFinal: async (config: any) => {
+    // Prevent webpack from using Storybook CSS rules to process CSS modules
+    config.module.rules.find(
+      (rule) => rule.test.toString() === "/\\.css$/"
+    ).exclude = /\.module\.css$/;
+
+    // Tell webpack what to do with CSS modules
+    config.module?.rules?.push({
+      test: /\.module\.css$/,
+      use: [
+        {
+          loader: "style-loader",
+          options: {
+            modules: {
+              namedExport: true,
+            },
+          },
+        },
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1,
+            modules: {
+              namedExport: true,
+              localIdentName: "[name]--[local]--[hash:base64:5]",
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, "../src"),
+    });
+
+    return config;
   },
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { ColorGuide } from "../styles/style-guide/ColorGuide";
+import { PrimaryCTA } from "../components/buttons/PrimaryCTA";
 
 const IndexPage: React.FC<PageProps> = () => {
   return <main className="">
-    <PrimaryCTA label={"Read more"} variant="blue" />
+    {/* <PrimaryCTA label={"Read more"} variant="blue" /> */}
   </main>;
 };
 
