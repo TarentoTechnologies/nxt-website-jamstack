8955efdc5aee1597d03b9be99f2e9ae1f5702989
SakthivelG <sakthivel.govindan@tarento.com>
2023-08-17 13:22:35 +0530

    feat: storybook configuration for CSS modules support

--- storybook-static/sb-addons/storybook-10/manager-bundle.js
+++
@@ -1,4 +0,0 @@
-try{
-var l=__STORYBOOKADDONS__,{addons:o,types:O,mockChannel:p}=__STORYBOOKADDONS__;var u=__STORYBOOKTHEMING__,{CacheProvider:S,ClassNames:b,Global:x,ThemeProvider:y,background:C,color:N,convert:k,create:a,createCache:R,createGlobal:B,createReset:D,css:G,darken:K,ensure:Y,ignoreSsrWarning:v,isPropValid:P,jsx:A,keyframes:E,lighten:H,styled:I,themes:M,typography:W,useTheme:j,withTheme:w}=__STORYBOOKTHEMING__;var s=a({base:"dark",brandTitle:"NXT Website design system"});o.setConfig({theme:s});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager-bundle.js.map
