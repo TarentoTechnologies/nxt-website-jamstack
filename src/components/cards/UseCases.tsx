4b4c14e537a46d83681b3f971017fac90431d97b
SakthivelG <sakthivel.govindan@tarento.com>
2023-11-21 17:55:30 +0530

    refactor: made usecases component values dynamic

--- src/components/cards/UseCases.tsx
+++ src/components/cards/UseCases.tsx
@@ -9,15 +9,11 @@ export const UseCases = ({ title, imgSrc }: UseCasesProps) => {
   return (
     <div className="layoutDetails d-feature useCases">
       <div className={`col-md-4 col-sm-6 col-xs-12 d-flex align-items-center`}>
-        <div className=" wow fadeInUp  animated">
-          <img
-            src="https://nxt.tarento.com/img/FAQs.svg"
-            alt="Frequently asking questions"
-            title="Frequently asking questions"
-          />
+        <div className="">
+          <img src={imgSrc} alt={title} title={title} />
         </div>
-        <div className="wow fadeInUp  animated">
-          <div className={useCasesSubTitle}>FAQs</div>
+        <div className="">
+          <div className={useCasesSubTitle}>{title}</div>
         </div>
       </div>
     </div>
