b8202f01bd1c12a8528db5f7dc470e2c3ffdaf8b
Abhijith K <abhijith.kizhakkethodi@tarento.com>
2023-12-15 17:45:29 +0530

    added navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import { navBarStyles } from "./Navbar.module.css";
+
+interface NavbarProps {
+  imgSrc: any;
+  imgAltText: any;
+  link: any;
+}
+
+export const Navbar = ({ imgSrc, imgAltText, link }: NavbarProps) => {
+  return (
+    <div className={`${navBarStyles}`}>
+      <a href={link}>
+        <img src={imgSrc} alt={imgAltText} title={imgAltText} />
+      </a>
+    </div>
+  );
+};
