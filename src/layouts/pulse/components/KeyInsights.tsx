fab89afa2d9cbb29f4d8df988dcf7090858dcef7
Abhijith K <abhijith.kizhakkethodi@tarento.com>
2023-12-21 16:38:17 +0530

    updated components in pulse page

--- src/layouts/pulse/components/KeyInsights.tsx
+++ src/layouts/pulse/components/KeyInsights.tsx
@@ -1,12 +1,19 @@
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
 import { PrimaryTitle } from "../../../components/titles/PrimaryTitle";
 import { keyFeature, keyImg, purpleBg } from "../Pulse.module.css";
 
 interface KeyInsightsProps {
   heading: string;
   cardList: any[];
+  isImage?: boolean;
 }
 
-export const KeyInsights = ({ heading, cardList }: KeyInsightsProps) => {
+export const KeyInsights = ({
+  heading,
+  cardList,
+  isImage,
+}: KeyInsightsProps) => {
   return (
     <div
       className={`container-fluid d-flex justify-content-center paddingTopBottom60 ${purpleBg}`}
@@ -18,12 +25,23 @@ export const KeyInsights = ({ heading, cardList }: KeyInsightsProps) => {
         <div className={`${keyFeature}`}>
           {cardList.map((item, index) => (
             <div key={index} className={`${keyImg}`}>
-              <img
-                src={item?.Image?.localFile?.url}
-                alt={item?.Image?.alternativeText}
-                title={item?.Image?.alternativeText}
-                className={`img-fluid`}
-              />
+              {!isImage ? (
+                <GatsbyImage
+                  image={
+                    getImage(item?.img?.localFile)
+                      ? getImage(item?.img?.localFile)
+                      : item?.img?.localFile
+                  }
+                  alt={""}
+                />
+              ) : (
+                <img
+                  src={item?.Image?.localFile?.url}
+                  alt={item?.Image?.alternativeText}
+                  title={item?.Image?.alternativeText}
+                  className={`img-fluid`}
+                />
+              )}
             </div>
           ))}
         </div>
