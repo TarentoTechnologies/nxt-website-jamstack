b43c9dfc7d6e25a97625f19341b6c628db59991f
Abhijith K <abhijith.kizhakkethodi@tarento.com>
2024-01-03 19:37:55 +0530

    integrated seo component

--- src/pages/lead.tsx
+++ src/pages/lead.tsx
@@ -5,6 +5,7 @@ import { useRecoilValue } from "recoil";
 import navbarImg from "../../static/images/logo-inner.svg";
 import { HeroBannerForTDI } from "../components/banners/HeroBannerForTDI";
 import { Navbar } from "../components/navbar/Navbar";
+import { Seo } from "../components/seo/Seo";
 import { FooterSection } from "../layouts/common";
 import {
   HeroSection,
@@ -203,6 +204,18 @@ export const query = graphql`
         CTAText
         CTALink
       }
+      seo {
+        metaTitle
+        metaDescription
+        canonicalURL
+        metaViewport
+        keywords
+        metaImage {
+          localFile {
+            url
+          }
+        }
+      }
     }
     enFooterSection: strapiFooterSection(locale: { eq: "en" }) {
       id
@@ -263,50 +276,16 @@ export const query = graphql`
 
 export default LeadPage;
 
-export const Head: HeadFC = () => (
-  <>
-    {/* Primary meta tags */}
-    <title>::NXT Tarento - LEAD::</title>
-    <link rel="canonical" href={`${process.env.GATSBY_SITE_URL}lead/`} />
-    <meta name="title" content="NXT Tarento - LEAD" />
-    <meta
-      name="description"
-      content="Design Thinking driven 'discovery & solutioning' program that helps businesses tackle complex problems and enables informed decision making in a short time."
+export const Head: HeadFC = ({ data }: LeadPageProps) => {
+  const currentLang = useRecoilValue(langSelectedAtom);
+  return (
+    <Seo
+      metaTitle={data[currentLang]?.seo?.metaTitle}
+      metaDesc={data[currentLang]?.seo?.metaDescription}
+      metaImg={data[currentLang]?.seo?.metaImage?.localFile?.url}
+      canonicalUrl={data[currentLang]?.seo?.canonicalURL}
+      metaViewport={data[currentLang]?.seo?.metaViewport}
+      keywords={data[currentLang]?.seo?.keywords}
     />
-    <meta
-      name="viewport"
-      content="width=device-width,height=device-height,initial-scale=1.0"
-    />
-
-    {/* Open graph */}
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content={`${process.env.GATSBY_SITE_URL}lead/`} />
-    <meta property="og:title" content="NXT Tarento - LEAD" />
-    <meta
-      property="og:description"
-      content="Design Thinking driven 'discovery & solutioning' program that helps businesses tackle complex problems and enables informed decision making in a short time."
-    />
-    <meta
-      property="og:image"
-      content={`${process.env.GATSBY_SITE_URL}static/4af750352a6c177c7f80d9ed351d244d/6998c/tech_solution_d8d031719f.png`}
-    />
-    <meta property="og:site_name" content="LEAD" />
-    <meta property="og:video" content="https://youtu.be/jrxB_PPKMAQ" />
-
-    {/* Twitter */}
-    <meta property="twitter:card" content="website" />
-    <meta
-      property="twitter:url"
-      content={`${process.env.GATSBY_SITE_URL}lead/`}
-    />
-    <meta property="twitter:title" content="NXT Tarento - LEAD" />
-    <meta
-      property="twitter:description"
-      content="Design Thinking driven 'discovery & solutioning' program that helps businesses tackle complex problems and enables informed decision making in a short time."
-    />
-    <meta
-      property="twitter:image"
-      content={`${process.env.GATSBY_SITE_URL}static/4af750352a6c177c7f80d9ed351d244d/6998c/tech_solution_d8d031719f.png`}
-    />
-  </>
-);
+  );
+};
